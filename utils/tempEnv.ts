0442d2cd634e3eb5ff7d6e4574203a1dad321dba
QQQCombo <rczhisawsome@gmail.com>
2025-09-24 08:53:23 +0800

    chore: added dev cloud url

--- utils/tempEnv.ts
+++ utils/tempEnv.ts
@@ -1,9 +1,12 @@
 // export const LOCAL_API_URL =
 // 	"http://10.10.6.92:5001/leafy-loader-444703-d0/us-central1";
-  
-// ZX's local
-export const LOCAL_API_URL = "http://10.10.6.177:5001/leafy-loader-444703-d0/us-central1";
-export const CLOUD_API_URL =
-	"https://us-central1-sibklcms-prod.cloudfunctions.net";
 
-export const apiBaseUrl = LOCAL_API_URL;
+// ZX's local
+export const LOCAL_API_URL =
+  "http://10.10.6.177:5001/leafy-loader-444703-d0/us-central1";
+export const CLOUD_API_URL =
+  "https://us-central1-leafy-loader-444703-d0.cloudfunctions.net";
+// export const CLOUD_API_URL =
+//   "https://us-central1-sibklcms-prod.cloudfunctions.net";
+
+export const apiBaseUrl = CLOUD_API_URL;
