8aa99f490a0dd19f05354c53d2cf4267dfeb4269
QQQCombo <rczhisawsome@gmail.com>
2026-04-28 22:16:21 +0800

    feat: update the qrScan and CreateSEssionSheets to use native wind and for the create and sign in attendance to use proper react query hook

--- services/CellAttendance/cellAttendance.service.ts
+++ services/CellAttendance/cellAttendance.service.ts
@@ -3,6 +3,43 @@ import { secureFetch } from "@/utils/secureFetch";
 import { ReturnVal } from "@/utils/types/returnVal.types";
 import { CellSession, CellSessionDetail } from "./cellAttendance.type";
 
+export const signInToCellSession = async (
+  cellId: number,
+  attendanceId: string,
+  peopleId: number,
+): Promise<void> => {
+  const response = await secureFetch(
+    apiEndpoints.cells.signInToSession(cellId),
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        attendance_id: attendanceId,
+        people_id: peopleId,
+      }),
+    },
+  );
+  const json: ReturnVal = await response.json();
+  if (!json.success) throw json;
+};
+
+export const createCellSession = async (
+  cellId: number,
+  meetingDate: string,
+): Promise<{ insertedId: number }> => {
+  const response = await secureFetch(apiEndpoints.cells.createSession(cellId), {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      cell_id: cellId,
+      meeting_date: meetingDate,
+    }),
+  });
+  const json: ReturnVal = await response.json();
+  if (!json.success) throw json;
+  return json.data;
+};
+
 export const fetchCellSessions = async (
   cellId: number,
 ): Promise<CellSession[]> => {
